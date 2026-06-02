5b9cf3cd636c9714c12b46c53611478968d294fd
matmilgrau <mateusamorimmaiali@gmail.com>
2026-06-02 08:45:02 -0300

    mensagem

--- script2.py
+++ script2.py
@@ -1,4 +1,6 @@
-espaços = {"começo" : {
+import json
+
+espacos = {"começo" : {
               "1" : {"lugar" : "casa", "pos" : [34,10]},
               "2" : {"lugar" : "igreja", "pos" : [5,10]},
               "3" : {"lugar" : "campos", "pos" : [20,18]}
@@ -15,10 +17,23 @@ espaços = {"começo" : {
               "1" : {"lugar" : "esquerda", "pos" : [5,10]},
               "2" : {"lugar" : "direito", "pos" : [34,10]},
               "3" : {"lugar" : "reto", "pos" : [20,18]}
-          }}
+          },
+          "mapa5" : {
+              "1" : {"lugar" : "julia", "pos" : [20,18]}}}
+
+interior = {"interior" :
+                { "1" : {"casa" : "carta escrita a mão", "pos" : [20,18]},
+                  "2" : {"igreja" : "padre", "pos" : [20,18]},
+                  }}
+
+x = input("Escolha a opção para interagir ")
+if (x=="casa"):
+    print(espacos["comeco"]["1"]["lugar"])
+
+
 LARGURA = 40
 ALTURA = 20
 
-print(espaços["começo"]["1"]["lugar"])
+print(espacos["mapa2"]["1"]["lugar"])
 #espaços vai chamar o dicionario, ai depois voce vai selecionar a chave entre o começo, mapa2, etc., depois, voce escolhe a chave entre os numeros e pois qual chave chamar
 #fazer mais um subdicionario para os espaços internos dos lugares, como casa, igreja, etc.
